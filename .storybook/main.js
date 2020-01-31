1fae5d468fba9831bfc4b14905dc28bcf7ed9322
Randell Quitain <>
2020-01-31 09:56:28 +0800

    Updated storybook version

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  stories: ['../stories/*.stories.(js|mdx)'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+    '@storybook/addon-viewport',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-a11y'
+  ]
+}
